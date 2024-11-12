bca0752b7631fe778cf598c9f89ccf1fbd8b70e9
Elif <elifxnur118@gmail.com>
2024-11-12 20:56:30 +0300

    started

---
+++ MyPortfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Typography, Button } from '@mui/material';
+import Navbar from './navbar'; // Navbar bileşenini içe aktar
+
+function App() {
+  return (
+    <>
+      <Navbar /> {/* Navbar'ı burada çağırıyoruz */}
+      <div style={{ textAlign: 'center', marginTop: '20px' }}>
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
